8f9d256103ae106baef6580f95b7a81e2825cc17
Joseph Cannova <jcannova@wheelership.com>
2019-06-09 23:02:21 -0400

    Build

---
+++ client/build/precache-manifest.2ea93d1bcf906874fb7c93830f96acae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b331561c79d7eb70badc0b2ec6f3b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f3cbc569ae6c15f509c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "919a32d3dd1fcc926618",
+    "url": "/static/js/2.7f82bd93.chunk.js"
+  },
+  {
+    "revision": "4f3cbc569ae6c15f509c",
+    "url": "/static/js/main.0e91f21a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
